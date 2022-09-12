460d746290f6e7214fa61b11ad1e8929a3f374b2
Camilla Johanson <51543468+camiruth@users.noreply.github.com>
2022-09-11 22:41:56 -0600

    Added Notes and Questions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//array list for the label and url. code given from teacher. This will be were I add new items to my table of contents each week.
 const links = [{
         label: "Week1 notes",
         url: "week1/index.html",
@@ -8,14 +9,19 @@ const links = [{
     },
 ];
 
+//Getting the ol list from the HTML code
 var ol = document.getElementById("list");
+
+//creating a loop to create the li and a elements
 for (var i = 0; i < links.length; i++) {
     var li = document.createElement("li");
     var a = document.createElement("a");
     
+    //references the array above to get the information
     a.href = links[i].url;
     a.innerHTML = links[i].label;
     
+    //puts the a inside the li and the li inside the ol. creating the element structure. 
     li.appendChild(a);
     ol.appendChild(li);
  }
